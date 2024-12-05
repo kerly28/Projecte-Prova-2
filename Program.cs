85b29a93fe74b3960dcbd93e3249147f9d9a69fa
kerly28 <kerly.guanoquiza.7e8@itb.cat>
2024-12-05 14:06:19 +0100

    Closes #5 - exercici 5

--- Program.cs
+++ Program.cs
@@ -1,41 +1,50 @@
 ﻿using System;
 using Prova;
 
-//PRE:L'usuari introdueix 3 numeros (dia, mes i any)
-namespace Exercici3
+//PRE:L'usuari introdueix 20 valors enters i quin valor vol buscar dins l'array
+namespace Exercici5
 {
     public class Program
     {
         public static void Main()
         {
-            //constants de missatges
-            const string InputDay = "Introdueix el dia: ";
-            const string InputMonth = "Introdueix el mes: ";
-            const string InputYear = "Introdueix l'any: ";
-            const string Validate= "La data introduida és vàlida";
-            const string NotValidate = "La data introduida no és vàlida";
+            const string Msg = "Introdueix 20 valors enters: ";
+            const string SearchNum = "Introdueix el valor que vols buscar: ";
+            const string Position = "El valor que vols buscar està a la posició: ";
+            const string NotFound = "El valor que vols buscar no està a l'array";
 
-            //demanen a l'usuari que introdueixi el dia, mes i any
-            Console.Write(InputDay);
-            int day = Convert.ToInt32(Console.ReadLine());
-            Console.Write(InputMonth);
-            int month = Convert.ToInt32(Console.ReadLine());
-            Console.Write(InputYear);
-            int year = Convert.ToInt32(Console.ReadLine());
-
-            //es criden els mètodes per validar el dia, mes i any
-            bool dayValid = ExerciciTres.Day(day);
-            bool monthValid = ExerciciTres.Month(month);
-            bool yearValid = ExerciciTres.YearValidate(year);
-            if (dayValid && monthValid && yearValid)
+            int[] array = new int[20];
+            Console.WriteLine(Msg);
+            int i;
+            //demanem a l'usari que introdueixi 20 valors enters
+            for (i = 0; i < 20; i++)
             {
-                Console.WriteLine(Validate);
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            //ordenem l'array introduit i mostrem l'array ordenat per pantalla
+            ExerciciCinc.Order(array, 0, 19);
+            Console.WriteLine("L'array ordenat és: ");
+            for (i = 0; i < 20; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine(SearchNum);
+            int key = Convert.ToInt32(Console.ReadLine());
+            //busquem el valor introduit per l'usuari dins l'array
+            ExerciciCinc.Order(array, 0, 19);
+            int pos = Array.BinarySearch(array, key);
+            if (pos >= 0)
+            {
+                Console.WriteLine(Position + pos);
             }
             else
             {
-                Console.WriteLine(NotValidate);
+                Console.WriteLine(NotFound);
+
+
             }
         }
     }
 }
-//POST:Es retorna si la data està dins el rang o no
+//POST:El programa mostra la llista ordenada ascendentment i si el valor que vol buscar estar dins l'array
